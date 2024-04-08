7d2a998af7beb418afe27261ffd311b053758ecd
Maxim Shestakov <maksim-shestakov-2004@mail.ru>
2024-04-08 12:17:23 +0300

    final version

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -5,14 +5,21 @@ import (
 	"net/http"
 	"strconv"
 
-	st "Effective-Mobile/internal/structures"
-	p "Effective-Mobile/pkg/postgresql"
+	"Effective-Mobile/internal/db"
 
 	"github.com/gin-gonic/gin"
 )
 
-func filterCars(regNum, mark, model string, cars []st.Car) []st.Car {
-	var filteredCars []st.Car
+type DBService interface {
+	AddCar(db.Car) error
+	DeleteCar(id string) error
+	UpdateCar(db.Car) error
+	GetCars() ([]db.Car, error)
+	AddOwner(db.Owner) error
+}
+
+func filterCars(regNum, mark, model string, cars []db.Car) []db.Car {
+	var filteredCars []db.Car
 
 	for _, car := range cars {
 		if (regNum == "" || car.Regnum == regNum) &&
@@ -34,44 +41,46 @@ func filterCars(regNum, mark, model string, cars []st.Car) []st.Car {
 // @Param regnum query string false "Registration number"
 // @Param mark query string false "Car mark"
 // @Param model query string false "Car model"
-// @Success 200 {object} st.StatusOKMessage "ok"
-// @Failure 500 {object} st.StatusInternalServerErrorMessage "internal server error"
-// @Failure 400 {object} st.StatusBadRequestMessage "bad request"
-// @Failure 404 {object} st.StatusNotFoundMessage "not found"
+// @Success 200 {object} db.StatusOKMessage "ok"
+// @Failure 500 {object} db.StatusInternalServerErrorMessage "internal server error"
+// @Failure 400 {object} db.StatusBadRequestMessage "bad request"
+// @Failure 404 {object} db.StatusNotFoundMessage "not found"
 // @Router /info [get]
-func GetCars(c *gin.Context) {
-	cars, err := p.GetCars()
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "can't get cars from db"})
-		return
-	}
-	filterRegnum := c.Query("regnum")
-	filterMark := c.Query("mark")
-	filterModel := c.Query("model")
-	pg, ok := c.GetQuery("page")
-	if !ok {
-		pg = "1"
-	}
-	page, _ := strconv.Atoi(pg)
-	lm, ok := c.GetQuery("limit")
-	if !ok {
-		lm = "20"
-	}
-	limit, _ := strconv.Atoi(lm)
+func GetCars(st db.Storage) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		cars, err := st.GetCars()
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "can't get cars from the db"})
+			return
+		}
+		filterRegnum := c.Query("regnum")
+		filterMark := c.Query("mark")
+		filterModel := c.Query("model")
+		pg, ok := c.GetQuery("page")
+		if !ok {
+			pg = "1"
+		}
+		page, _ := strconv.Atoi(pg)
+		lm, ok := c.GetQuery("limit")
+		if !ok {
+			lm = "20"
+		}
+		limit, _ := strconv.Atoi(lm)
 
-	start := (page - 1) * limit
-	end := page * limit
+		start := (page - 1) * limit
+		end := page * limit
 
-	cars = filterCars(filterRegnum, filterMark, filterModel, cars)
-	if end > len(cars) {
-		end = len(cars)
+		cars = filterCars(filterRegnum, filterMark, filterModel, cars)
+		if end > len(cars) {
+			end = len(cars)
+		}
+
+		PaginatedCars := cars[start:end]
+
+		log.Println("cars received") //debug log
+		c.JSON(http.StatusOK, PaginatedCars)
 	}
-	
-	PaginatedCars := cars[start:end]
-
-	log.Println("cars received") //debug log
-	c.JSON(http.StatusOK, PaginatedCars)
 }
 
 // CreateCar godoc
@@ -81,27 +90,28 @@ func GetCars(c *gin.Context) {
 // @ID create-car
 // @Accept json
 // @Produce json
-// @Param input body st.Car true "Car info (regnum, mark, model, owner_id are required)."
-// @Success 200 {object} st.StatusOKMessage "ok"
-// @Failure 500 {object} st.StatusInternalServerErrorMessage "internal server error"
-// @Failure 400 {object} st.StatusBadRequestMessage "bad request"
-// @Failure 404 {object} st.StatusNotFoundMessage "not found"
+// @Param input body db.Car true "Car info (regnum, mark, model, owner_id are required)."
+// @Success 200 {object} db.StatusOKMessage "ok"
+// @Failure 500 {object} db.StatusInternalServerErrorMessage "internal server error"
+// @Failure 400 {object} db.StatusBadRequestMessage "bad request"
+// @Failure 404 {object} db.StatusNotFoundMessage "not found"
 // @Router /cars [post]
-func CreateCar(c *gin.Context) {
-	var car st.Car
-	if err := c.ShouldBindJSON(&car); err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "can't bind json body to struct"})
-		return
+func CreateCar(st db.Storage) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var car db.Car
+		if err := c.ShouldBindJSON(&car); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "can't bind json body to struct"})
+			return
+		}
+		if err := st.AddCar(car); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "can't add a car to the db"})
+			return
+		}
+		log.Println("car created") //debug log
+		c.JSON(http.StatusOK, gin.H{"message": "car created"})
 	}
-	err := p.AddCar(car)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "can't add a car to the db"})
-		return
-	}
-	log.Println("car created") //debug log
-	c.JSON(http.StatusOK, gin.H{"message": "car created"})
 }
 
 // UpdateCar godoc
@@ -111,27 +121,29 @@ func CreateCar(c *gin.Context) {
 // @ID update-car
 // @Accept json
 // @Produce json
-// @Param input body st.Car true "Change car info, ID and other one or more fields are required. If the field is empty, it will not be changed ."
-// @Success 200 {object} st.StatusOKMessage "ok"
-// @Failure 500 {object} st.StatusInternalServerErrorMessage "internal server error"
-// @Failure 400 {object} st.StatusBadRequestMessage "bad request"
-// @Failure 404 {object} st.StatusNotFoundMessage "not found"
+// @Param input body db.Car true "Change car info, ID and other one or more fields are required. If the field is empty, it will not be changed ."
+// @Success 200 {object} db.StatusOKMessage "ok"
+// @Failure 500 {object} db.StatusInternalServerErrorMessage "internal server error"
+// @Failure 400 {object} db.StatusBadRequestMessage "bad request"
+// @Failure 404 {object} db.StatusNotFoundMessage "not found"
 // @Router /cars/{id} [put]
-func UpdateCar(c *gin.Context) {
-	var car st.Car
-	if err := c.ShouldBindJSON(&car); err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "can't bind json body to struct"})
-		return
+func UpdateCar(st db.Storage) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var car db.Car
+		if err := c.ShouldBindJSON(&car); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "can't bind json body to struct"})
+			return
+		}
+
+		if err := st.UpdateCar(car); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "can't update a car in the db"})
+			return
+		}
+		log.Println("car updated") //debug log
+		c.JSON(http.StatusOK, gin.H{"message": "car updated"})
 	}
-	err := p.UpdateCar(car)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "can't update a car in the db"})
-		return
-	}
-	log.Println("car updated") //debug log
-	c.JSON(http.StatusOK, gin.H{"message": "car updated"})
 }
 
 // DeleteCar godoc
@@ -142,21 +154,22 @@ func UpdateCar(c *gin.Context) {
 // @Accept json
 // @Produce json
 // @Param id path string true "Car ID"
-// @Success 200 {object} st.StatusOKMessage "ok"
-// @Failure 500 {object} st.StatusInternalServerErrorMessage "internal server error"
-// @Failure 400 {object} st.StatusBadRequestMessage "bad request"
-// @Failure 404 {object} st.StatusNotFoundMessage "not found"
+// @Success 200 {object} db.StatusOKMessage "ok"
+// @Failure 500 {object} db.StatusInternalServerErrorMessage "internal server error"
+// @Failure 400 {object} db.StatusBadRequestMessage "bad request"
+// @Failure 404 {object} db.StatusNotFoundMessage "not found"
 // @Router /cars/{id} [delete]
-func DeleteCar(c *gin.Context) {
-	id := c.Param("id")
-	err := p.DeleteCar(id)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "can't delete a car from the db"})
-		return
+func DeleteCar(st db.Storage) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		if err := st.DeleteCar(id); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "can't delete a car from the db"})
+			return
+		}
+		log.Println("car deleted") //debug log
+		c.JSON(http.StatusOK, gin.H{"message": "car deleted"})
 	}
-	log.Println("car deleted") //debug log
-	c.JSON(http.StatusOK, gin.H{"message": "car deleted"})
 }
 
 // AddOwner godoc
@@ -166,25 +179,27 @@ func DeleteCar(c *gin.Context) {
 // @ID add-owner
 // @Accept json
 // @Produce json
-// @Param input body st.Owner true "Owner info (only name, surname are required)."
-// @Success 200 {object} st.StatusOKMessage "ok"
-// @Failure 500 {object} st.StatusInternalServerErrorMessage "internal server error"
-// @Failure 400 {object} st.StatusBadRequestMessage "bad request"
-// @Failure 404 {object} st.StatusNotFoundMessage "not found"
+// @Param input body db.Owner true "Owner info (only name, surname are required)."
+// @Success 200 {object} db.StatusOKMessage "ok"
+// @Failure 500 {object} db.StatusInternalServerErrorMessage "internal server error"
+// @Failure 400 {object} db.StatusBadRequestMessage "bad request"
+// @Failure 404 {object} db.StatusNotFoundMessage "not found"
 // @Router /owners [post]
-func AddOwner(c *gin.Context) {
-	var owner st.Owner
-	if err := c.ShouldBindJSON(&owner); err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "can't bind json body to struct"})
-		return
+func AddOwner(st db.Storage) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var owner db.Owner
+		if err := c.ShouldBindJSON(&owner); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "can't bind json body to struct"})
+			return
+		}
+
+		if err := st.AddOwner(owner); err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "can't add an owner to the db"})
+			return
+		}
+		log.Println("owner created") //debug log
+		c.JSON(http.StatusOK, gin.H{"message": "owner created"})
 	}
-	err := p.AddOwner(owner)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "can't add an owner to the db"})
-		return
-	}
-	log.Println("owner created") //debug log
-	c.JSON(http.StatusOK, gin.H{"message": "owner created"})
 }
